76aedb42a5556f1a46a74ffa1a9120a58df4572b
Bikrom-Roy <bikromroy2001@gmail.com>
2022-12-11 02:27:46 +0600

    project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,27 +1,177 @@
 import React from 'react';
+import images from '../accests/contact/Motor Complex-second-hand-products-selling (1).png';
 
 const Project = () => {
     return (
         <div>
-            <p className='font-inter text-2xl all-heading'> <span className='logo-color mr-3'> 03. </span> Some Things I’ve Built </p>
+            <p className='font-inter px-4 md:px-16 text-2xl all-heading'> <span className='logo-color mr-3'> 03. </span> Some Things I’ve Built </p>
 
-            <div className="p-5 mx-auto sm:p-10 md:p-16 dark:bg-gray-800 dark:text-gray-100">
-                <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
-                    <img src="https://source.unsplash.com/random/480x360" alt="" className="w-full h-60 sm:h-96 dark:bg-gray-500" />
-                    
-                    <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-900">
-                        <div className="space-y-2">
-                            <a rel="noopener noreferrer" href="#" className="inline-block text-2xl font-semibold sm:text-3xl">The Best Activewear from the Nordstrom Anniversary Sale</a>
-                            <p className="text-xs dark:text-gray-400">By
-                                <a rel="noopener noreferrer" href="#" className="text-xs hover:underline">Leroy Jenkins</a>
+            <section>
+                <div className="container flex flex-col p-6 mx-auto sm:py-10 lg:py-12 lg:flex-row ">
+                    <div className="flex  items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                        <img src='https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="" className="object-contain h-72 sm:h-80 lg:h-96 w-auto  xl:h-112 2xl:h-128 rounded-md" />
+                    </div>
+                    <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+
+                        <p className='text-right font-mono logo-color'>
+                            Featured Project
+                        </p>
+                        <a className='cursor-pointer' target='_blank' href="https://motor-complex.web.app/"><h3 className='text-right mb-5 font-inter font-bold text-2xl cursor-pointer'>Buy Second-Hand Cars</h3></a>
+
+                        <div className=" p-6 text-right  project-details">
+                            <p className='flex font-mono items-start gap-3 mb-2'>
+
+                                <span className='text-justify text-sm'> Here one can see category-wise cars and book cars. and car booking management system website.</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#64ffda" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
+                            </p>
+                            <p className='flex font-mono items-start gap-3 mb-2'>
+
+                                <span className='text-justify text-sm'> Here is the admin dashboard. Services where an admin can add or remove a user and manage to book.</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#64ffda" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
                             </p>
                         </div>
-                        <div className="dark:text-gray-100">
-                            <p>Insert the actual text content here...</p>
+                        <div className='flex mt-5 mb-5 items-center flex-wrap justify-end font-mono gap-4'>
+                            <p className='header-hover cursor-pointer text-sm'>Tailwind CSS</p>
+                            <p className='header-hover text-sm cursor-pointer'>React.JS</p>
+                            <p className='header-hover text-sm cursor-pointer'>Node.JS</p>
+                            <p className='header-hover text-sm cursor-pointer'>Express.JS</p>
+                            <p className='header-hover cursor-pointer text-sm'>Mongodb</p>
                         </div>
+                        <div className='flex items-center gap-4 justify-end'>
+                            <a href="https://github.com/Bikrom2001/motor-complex-client" target='_blank'>
+                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github w-6 h-6 cursor-pointer header-hover"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                            </a>
+                            <a href="https://motor-complex.web.app/" target='_blank'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 header-hover cursor-pointer h-6">
+                                    <title>Live-side</title>
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                </svg>
+                            </a>
+
+                        </div>
+
                     </div>
                 </div>
-            </div>
+            </section>
+
+            <section>
+                <div className="container flex flex-col p-6 mx-auto sm:py-10 lg:py-12 lg:flex-row ">
+
+                    <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+
+                        <p className='text-left font-mono logo-color'>
+                            Featured Project
+                        </p>
+                        <a className='cursor-pointer' target='_blank' href="https://doctors-services-1fefe.web.app/"><h3 className='text-left mb-5 font-inter font-bold text-2xl cursor-pointer'>Doctors Portal</h3></a>
+
+                        <div className=" p-6 text-right  project-details">
+                            <p className='flex font-mono items-start gap-3 mb-2'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#64ffda" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
+
+                                <span className='text-justify text-sm'> Doctors portal including an add or remove service and appointment management portal with firebase login.</span>
+
+                            </p>
+                            <p className='flex font-mono items-start gap-3 mb-2'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#64ffda" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
+                                <span className='text-justify text-sm'> Form to leave reviews for the clients to show on Homepages and used private routes or admin panel.</span>
+
+                            </p>
+                        </div>
+                        <div className='flex mt-5 mb-5 items-center flex-wrap justify-start font-mono gap-4'>
+                            <p className='header-hover cursor-pointer text-sm'>Tailwind CSS</p>
+                            <p className='header-hover text-sm cursor-pointer'>React.JS</p>
+                            <p className='header-hover text-sm cursor-pointer'>Node.JS</p>
+                            <p className='header-hover text-sm cursor-pointer'>Express.JS</p>
+                            <p className='header-hover cursor-pointer text-sm'>Mongodb</p>
+                        </div>
+                        <div className='flex items-center gap-4 justify-start'>
+                            <a href="https://github.com/Bikrom2001/life-of-travel-client" target='_blank'>
+                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github w-6 h-6 cursor-pointer header-hover"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                            </a>
+                            <a href="https://doctors-services-1fefe.web.app/" target='_blank'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 header-hover cursor-pointer h-6">
+                                    <title>Live-side</title>
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                </svg>
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                    <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                        <img src="https://images.pexels.com/photos/326502/pexels-photo-326502.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 rounded-md" />
+                    </div>
+
+                </div>
+            </section>
+
+            <section>
+                <div className="container flex flex-col p-6 mx-auto sm:py-10 lg:py-12 lg:flex-row ">
+                    <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                        <img src="https://images.pexels.com/photos/2857477/pexels-photo-2857477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 rounded-md" />
+                    </div>
+                    <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+
+                        <p className='text-right font-mono logo-color'>
+                            Featured Project
+                        </p>
+                        <a className='cursor-pointer' target='_blank' href="https://global-online-courses.web.app/"><h3 className='text-right mb-5 font-inter font-bold text-2xl cursor-pointer'>Online Courses</h3></a>
+
+                        <div className=" p-6 text-right  project-details">
+                            <p className='flex font-mono items-start gap-3 mb-2'>
+
+                                <span className='text-justify text-sm'> This is an Online Courses web app where you can learn more about Blockchain and firebase login systems.</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#64ffda" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
+                            </p>
+                            <p className='flex font-mono items-start gap-3 mb-2'>
+
+                                <span className='text-justify text-sm'> Click the download button to download the course and the user login profile create or the user image.</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#64ffda" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className='flex mt-5 mb-5 items-center flex-wrap justify-end font-mono gap-4'>
+                            <p className='header-hover cursor-pointer text-sm'>Tailwind CSS</p>
+                            <p className='header-hover text-sm cursor-pointer'>React.JS</p>
+                            <p className='header-hover text-sm cursor-pointer'>Node.JS</p>
+                            <p className='header-hover text-sm cursor-pointer'>Express.JS</p>
+                            <p className='header-hover cursor-pointer text-sm'>Mongodb</p>
+                        </div>
+                        <div className='flex items-center gap-4 justify-end'>
+                            <a href="https://github.com/Bikrom2001/programming-lang-client-side" target='_blank'>
+                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github w-6 h-6 cursor-pointer header-hover"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                            </a>
+                            <a href="https://global-online-courses.web.app/" target='_blank'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 header-hover cursor-pointer h-6">
+                                    <title>Live-side</title>
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                </svg>
+                            </a>
+
+                        </div>
+                       <div className='flex justify-end mt-5'>
+                       <button className='px-6 w-38 text-center py-4 m-2 custom-btn '>
+                            Learn More
+                        </button>
+                       </div>
+
+                    </div>
+                </div>
+
+            </section>
+
         </div>
     );
 };
